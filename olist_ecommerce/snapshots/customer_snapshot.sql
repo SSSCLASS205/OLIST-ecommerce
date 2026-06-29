7513b7dcd84f7561dbaeca4d5fc8540f1c377b3c
Amr Gaber <sssclass205@gmail.com>
2026-06-29 15:46:08 -0700

    test#4

--- olist_ecommerce/snapshots/customer_snapshot.sql
+++ olist_ecommerce/snapshots/customer_snapshot.sql
@@ -3,14 +3,12 @@
 {{
     config(
         target_database='OLIST_WAREHOUSE',
-        target_schema={{ target.schema ~ '_SILVER' if target.name != 'prod' else 'SILVER' }},
+        target_schema=target.schema ~ '_SILVER' if target.name != 'prod' else 'SILVER',
         unique_key='customer_id',
         strategy='check',
         check_cols=['customer_city', 'customer_state', 'customer_zip_code_prefix']
     )
 }}
-
-
 
 SELECT 
     customer_id,
@@ -20,6 +18,5 @@ SELECT
     customer_state,
     _airbyte_emitted_at
 FROM {{ source('BRONZE', 'customers_bronze') }}
-
 
 {% endsnapshot %}
